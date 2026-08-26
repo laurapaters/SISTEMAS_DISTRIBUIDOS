4cc1db21c588cc4acd1a7f428acf1c14bbf86cf6
laurapaters <33295703+laurapaters@users.noreply.github.com>
2026-08-26 16:33:07 -0500

    Add files via upload

---
+++ Tema_5/Codigo de ejemplo/unir-delivery/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Footer = () => {
+
+
+
+    console.log("montaje de Footer");
+
+
+    return (
+        <footer className="header-footer">
+            <p className="header-footer-text">© 2023 UNIR Delivery.</p>
+        </footer>
+    );
+}
---
+++ Tema_5/Codigo de ejemplo/unir-delivery/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Header = () => {
+
+
+
+
+
+
+    return (
+        <header className="header-footer">
+            <h1 className="header-footer-text">Bienvenidos a UNIR Delivery</h1>
+        </header>
+    );
+}
